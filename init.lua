9fe9a4226db0627a6078bdf8e31562df6a62ec69
Thibaut Tauveron <thibaut.tauveron@gmail.com>
2024-03-19 23:37:45 +0100

    update wilder

--- init.lua
+++ init.lua
@@ -723,27 +723,66 @@ wilder.set_option('pipeline', {
     wilder.cmdline_pipeline({
       fuzzy = 1,
       fuzzy_filter = wilder.lua_fzy_filter(),
+      debounce = 10,
     }),
     wilder.vim_search_pipeline()
   )
 })
 
+local gradient = {
+  '#f4468f', '#fd4a85', '#ff507a', '#ff566f', '#ff5e63',
+  '#ff6658', '#ff704e', '#ff7a45', '#ff843d', '#ff9036',
+  '#f89b31', '#efa72f', '#e6b32e', '#dcbe30', '#d2c934',
+  '#c8d43a', '#bfde43', '#b6e84e', '#aff05b'
+}
+
+for i, fg in ipairs(gradient) do
+  gradient[i] = wilder.make_hl('WilderGradient' .. i, 'Pmenu', {{a = 1}, {a = 1}, {foreground = fg}})
+end
+
+local highlighters = {
+  wilder.highlighter_with_gradient({
+    wilder.pcre2_highlighter(),
+    wilder.lua_fzy_highlighter(),
+  }),
+}
+
+local popupmenu_renderer = wilder.popupmenu_renderer(
+  wilder.popupmenu_border_theme({
+    border = 'rounded',
+    empty_message = wilder.popupmenu_empty_message_with_spinner(),
+    highlighter = highlighters,
+    highlights = {
+      border = 'Normal',
+      gradient = gradient,
+    },
+     left = {
+       ' ',
+      wilder.popupmenu_devicons(),
+      -- wilder.popupmenu_buffer_flags({
+      --   flags = ' a + ',
+      --   icons = {['+'] = '', a = '', h = ''},
+      -- }),
+     },
+     right = {
+       ' ',
+      wilder.popupmenu_scrollbar(),
+     },
+  })
+)
+
+local wildmenu_renderer = wilder.wildmenu_renderer({
+  highlighter = highlighters,
+  separator = ' · ',
+  left = {' ', wilder.wildmenu_spinner(), ' '},
+  right = {' ', wilder.wildmenu_index()},
+})
+
 wilder.set_option('renderer', wilder.renderer_mux({
-  [':'] = wilder.popupmenu_renderer({
-    highlighter = wilder.lua_fzy_highlighter(),
-    left = {
-      ' ',
-      wilder.popupmenu_devicons()
-    },
-    right = {
-      ' ',
-      wilder.popupmenu_scrollbar()
-    },
-  }),
-  ['/'] = wilder.wildmenu_renderer({
-    highlighter = wilder.lua_fzy_highlighter(),
-  }),
-}))
+  [':'] = popupmenu_renderer,
+  ['/'] = wildmenu_renderer,
+  substitute = wildmenu_renderer,
+ }))
 -----------------------------------
 
 -- The line beneath this is called `modeline`. See `:help modeline`
