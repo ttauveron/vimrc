4a591c2b70c78523eef8039f84c4872563c615cd
Thibaut Tauveron <thibaut.tauveron@gmail.com>
2024-05-08 01:30:11 +0200

    add nvim-surround

--- init.lua
+++ init.lua
@@ -246,6 +246,16 @@ require('lazy').setup({
       require('oil').setup()
       vim.keymap.set('n', '<leader>-', require('oil').open, { noremap = true, silent = true, desc = 'Open oil' })
     end
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
   }
 }, {})
 
